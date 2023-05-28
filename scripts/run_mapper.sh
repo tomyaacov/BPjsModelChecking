61896885ad9a93247dd4570636e6fe3b8e0131f9
tomyaacov <tomyaacov1210@gmail.com>
2023-05-28 10:16:26 +0300

    updating mapper batch file

--- scripts/run_mapper.sh
+++ scripts/run_mapper.sh
@@ -6,7 +6,7 @@
 #SBATCH --output run_mapper.out ### output log for running job - %J is the job number variable
 #SBATCH --mail-user=tomya@post.bgu.ac.il ### users email for sending job status notifications ñ replace with yours
 #SBATCH --mail-type=BEGIN,END,FAIL ### conditions when to send the email. ALL,BEGIN,END,FAIL, REQUEU, NONE
-#SBATCH --mem=250G ### total amount of RAM // 500
+#SBATCH --mem=500G ### total amount of RAM // 500
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32 ##. // max 128
 
@@ -14,7 +14,7 @@
 module load anaconda ### load anaconda module
 source activate bppy_model_checking ### activating Conda environment. Environment must be configured before running the job
 cd ~/repos/BPjsModelChecking/ || exit
-export MAVEN_OPTS="-Xms250g -Xmx250g"
+export MAVEN_OPTS="-Xms500g -Xmx500g"
 mvn compile > /dev/null 2>&1
 #options=(
 #"hot_cold 30 1 true" "hot_cold 60 1 true" "hot_cold 90 1 true"
@@ -29,12 +29,12 @@ mvn compile > /dev/null 2>&1
 options=(
 "hot_cold 60 3 true" "hot_cold 90 3 true"
 
-"dining_philosophers 3 true" "dining_philosophers 6 true" "dining_philosophers 9 true" "dining_philosophers 12 true" "dining_philosophers 15 true"
+"dining_philosophers 12 true" "dining_philosophers 15 true"
 
-"ttt 2 2 true" "ttt 4 4 true" "ttt 5 5 true"
+"ttt 4 4 true"
 
 )
 for option in "${options[@]}"; do
   echo "$option"
-  timeout 480m mvn exec:java -D"exec.mainClass"="il.ac.bgu.cs.bp.bpjsmodelchecking.RunMapper" -D"exec.args"="$option"
+  timeout 1000m mvn exec:java -D"exec.mainClass"="il.ac.bgu.cs.bp.bpjsmodelchecking.RunMapper" -D"exec.args"="$option"
 done
