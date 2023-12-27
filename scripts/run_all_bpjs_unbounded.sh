1e766983f663441de0e5c69ed4275835d2156b7e
tomyaacov <tomyaacov1210@gmail.com>
2023-12-27 15:09:23 +0200

    updating experiment

--- scripts/run_all_bpjs_unbounded.sh
+++ scripts/run_all_bpjs_unbounded.sh
@@ -6,7 +6,7 @@
 #SBATCH --output run_all_bpjs_unbounded.out ### output log for running job - %J is the job number variable
 #SBATCH --mail-user=tomya@post.bgu.ac.il ### users email for sending job status notifications ñ replace with yours
 #SBATCH --mail-type=BEGIN,END,FAIL ### conditions when to send the email. ALL,BEGIN,END,FAIL, REQUEU, NONE
-#SBATCH --mem=16G ### total amount of RAM // 500
+#SBATCH --mem=32G ### total amount of RAM // 500
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32 ##. // max 128
 
@@ -15,24 +15,24 @@ module load anaconda ### load anaconda module
 source activate bppy_model_checking ### activating Conda environment. Environment must be configured before running the job
 cd ~/repos/BPjsModelChecking/ || exit
 ulimit -s unlimited
-export MAVEN_OPTS="-Xms1024k -Xmx4g"
+export MAVEN_OPTS="-Xms1024k -Xmx16g"
 mvn compile > /dev/null 2>&1
 options=(
 "hot_cold 30 1 true false" "hot_cold 60 1 true false" "hot_cold 90 1 true false"
 "hot_cold 30 2 true false" "hot_cold 60 2 true false" "hot_cold 90 2 true false"
 "hot_cold 30 3 true false" "hot_cold 60 3 true false" "hot_cold 90 3 true false"
 
-"dining_philosophers 3 true false" "dining_philosophers 6 true false" "dining_philosophers 9 true false" "dining_philosophers 12 true false" "dining_philosophers 15 true false"
+"dining_philosophers 3 true false" "dining_philosophers 6 true false" "dining_philosophers 9 true false" "dining_philosophers 12 true false"
 
-"ttt 2 2 true false" "ttt 3 3 true false" "ttt 4 4 true false"
+"ttt 3 3 true false" "ttt 4 4 true false"
 )
 
 echo "option,run,time,memory" > run_all_bpjs_unbounded_output.csv
 for option in "${options[@]}"; do
   echo "$option"
-  for i in {1..5}
+  for i in {1..10}
   do
-    timeout 30m /usr/bin/time -a -o run_all_bpjs_unbounded_output.csv -f "$option,$i,%E,%M" mvn exec:java -D"exec.args"="$option"
+    timeout 60m /usr/bin/time -a -o run_all_bpjs_unbounded_output.csv -f "$option,$i,%E,%M" mvn exec:java -D"exec.args"="$option"
     EXIT_STATUS=$?
     if [ $EXIT_STATUS -eq 124 ]
     then
